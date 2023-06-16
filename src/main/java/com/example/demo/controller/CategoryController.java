37a6ffbcd8af1af2cf5ccc535245bea4347d09b3
unknown <nguyendongminhtri29092014@gmail.com>
2023-06-16 13:39:59 +0700

    update pagination

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -5,11 +5,13 @@ import com.example.demo.model.Category;
 import com.example.demo.service.category.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RequestMapping("/category")
 @CrossOrigin(origins = "*")
@@ -17,20 +19,48 @@ import javax.validation.Valid;
 public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
+
     @GetMapping
-    public ResponseEntity<?> showListCategory(){
+    public ResponseEntity<?> showListCategory() {
         return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
+
     @GetMapping("/page")
-    public ResponseEntity<?> pageCategory(Pageable pageable){
+    public ResponseEntity<?> pageCategory(@PageableDefault(size = 3) Pageable pageable) {
         return new ResponseEntity<>(categoryService.findAll(pageable), HttpStatus.OK);
     }
+
     @PostMapping()
-    public ResponseEntity<?> createCategory( @RequestBody Category category){
-        if(categoryService.existsByName(category.getName())){
+    public ResponseEntity<?> createCategory(@RequestBody Category category) {
+        if (categoryService.existsByName(category.getName())) {
             return new ResponseEntity<>(new ResponMessage("name_existed"), HttpStatus.OK);
         }
         categoryService.save(category);
         return new ResponseEntity<>(new ResponMessage("creat_success"), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detailCategory(@PathVariable Long id) {
+        Optional<Category> category = categoryService.findById(id);
+        if (!category.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Optional<Category> category1 = categoryService.findById(id);
+        if (!category1.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (!category.getAvatar().equals(category1.get().getAvatar())) {
+            category.setId(category1.get().getId());
+        } else if (categoryService.existsByName(category.getName())) {
+            return new ResponseEntity<>(new ResponMessage("name_existed"), HttpStatus.OK);
+        }
+        category.setId(category1.get().getId());
+        categoryService.save(category);
+        return new ResponseEntity<>(new ResponMessage("update_success"), HttpStatus.OK);
     }
 }
